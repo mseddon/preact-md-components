aeb6a14056412ad7ca40e5b926c5538dcfd6ff76
mseddon <matt@scryetek.com>
2017-01-09 10:32:13 +0000

    Tweak git/npm ignores

---
+++ src/client/lib.tsx
@@ -0,0 +1,16 @@
+import { waitForFonts } from "./domutil";
+import { createStore, Store, Reducer } from "redux";
+import * as theme from "./theme";
+
+export function makeStore<A>(reducer: Reducer<A>): Store<A> {
+    if(process.env["DEBUG"])
+        return createStore(reducer, window["__REDUX_DEVTOOLS_EXTENSION__"] && window["__REDUX_DEVTOOLS_EXTENSION__"]());
+    else
+        return createStore(reducer);
+}
+
+export function init(renderFn: () => void) {
+    if(process.env["DEBUG"])
+        require('preact/devtools')
+    waitForFonts(["Roboto"], renderFn, () => {}
+)};
