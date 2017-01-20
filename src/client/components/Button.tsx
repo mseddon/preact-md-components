99a89a9ef6057546352fb8040ddf96cbe542102a
mseddon <matt@scryetek.com>
2017-01-20 16:53:53 +0000

    Don't trigger action when disabled

--- src/client/components/Button.tsx
+++ src/client/components/Button.tsx
@@ -5,7 +5,7 @@ require('../theme');
 
 export class Button extends Component<{disabled?: boolean, onClick: () => void, extraClasses?: string}, {}> {
     render() {
-        return <div class={"md-button" + (this.props.extraClasses ? " "+this.props.extraClasses : "")} onClick={this.props.onClick}>{ this.props.disabled ? null : <RippleBox/> }
+        return <div class={"md-button" + (this.props.extraClasses ? " "+this.props.extraClasses : "")} onClick={() => this.props.disabled ? null : this.props.onClick()}>{ this.props.disabled ? null : <RippleBox/> }
                   <button onClick={() => this.props.disabled ? null : this.props.onClick()} disabled={this.props.disabled} className={"accent-bg"}>{this.props.children}</button>
                </div>
     }
