e9cdff06206daba521cc0d9f9fcd20de84bf2ecf
mseddon <matt@scryetek.com>
2017-01-20 16:49:08 +0000

    allow extraClasses in md-button

--- src/client/components/Button.tsx
+++ src/client/components/Button.tsx
@@ -3,8 +3,10 @@ import "./styles.less";
 import { RippleBox } from "./RippleBox"
 require('../theme');
 
-export class Button extends Component<{disabled?: boolean, onClick: () => void}, {}> {
+export class Button extends Component<{disabled?: boolean, onClick: () => void, extraClasses?: string}, {}> {
     render() {
-        return <div class="md-button" onClick={this.props.onClick}>{ this.props.disabled ? null : <RippleBox/> }<button onClick={this.props.onClick} disabled={this.props.disabled} className="accent-bg">{this.props.children}</button></div>
+        return <div class={"md-button" + (this.props.extraClasses ? " "+this.props.extraClasses : "")} onClick={this.props.onClick}>{ this.props.disabled ? null : <RippleBox/> }
+                  <button onClick={() => this.props.disabled ? null : this.props.onClick()} disabled={this.props.disabled} className={"accent-bg"}>{this.props.children}</button>
+               </div>
     }
 }
