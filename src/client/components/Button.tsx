926000bc4757027146a922869475fa85407bdf0b
mseddon <matt@scryetek.com>
2017-02-08 16:44:34 +0000

    type='flat' on Button

--- src/client/components/Button.tsx
+++ src/client/components/Button.tsx
@@ -3,10 +3,10 @@ import "./styles.less";
 import { RippleBox } from "./RippleBox"
 require('../theme');
 
-export class Button extends Component<{disabled?: boolean, onClick: () => void, extraClasses?: string}, {}> {
+export class Button extends Component<{disabled?: boolean, type?: "accent" | "flat", onClick: () => void, extraClasses?: string}, {}> {
     render() {
-        return <div class={"md-button" + (this.props.extraClasses ? " "+this.props.extraClasses : "")} onClick={() => this.props.disabled ? null : this.props.onClick()}>{ this.props.disabled ? null : <RippleBox/> }
-                  <button onClick={() => this.props.disabled ? null : this.props.onClick()} disabled={this.props.disabled} className={"accent-bg"}>{this.props.children}</button>
+        return <div class={"md-button" + (this.props.extraClasses ? " "+this.props.extraClasses : "")} onClick={() => this.props.disabled ? null : this.props.onClick()}>{ this.props.disabled ? null : <RippleBox extraClasses={(this.props.type && this.props.type == "flat") ? "dark-ripple": ""}/> }
+                  <button onClick={() => this.props.disabled ? null : this.props.onClick()} disabled={this.props.disabled} className={(this.props.type && this.props.type == "flat") ? "accent-fg" : "accent-bg"}>{this.props.children}</button>
                </div>
     }
 }
