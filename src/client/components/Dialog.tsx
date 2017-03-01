43b32268fe2648094dd3447f444cd06cf3884cfd
coreyauger <coreyauger@gmail.com>
2017-03-01 09:42:44 -0800

    fixed dialog layer covering screen

--- src/client/components/Dialog.tsx
+++ src/client/components/Dialog.tsx
@@ -8,7 +8,7 @@ export class Dialog extends Component<{title?: string, buttons?: VNode[], modal?
     render() {
         return <div>
             {this.props.show ? [
-                <Portal into="#md-layer-dialog"><div className="md-dialog"><h1>{this.props.title}</h1><div className="md-dialog-content">{this.props.children}</div><div className="right-icons">{this.props.buttons}</div></div></Portal>,
+                <Portal into="#md-layer-dialog"><div className="md-dialog-center"><div className="md-dialog"><h1>{this.props.title}</h1><div className="md-dialog-content">{this.props.children}</div><div className="right-icons">{this.props.buttons}</div></div></div></Portal>,
                 <Portal into="body"><div className="md-blackout" onClick={() => {if(!this.props.modal && this.props.onclose)this.props.onclose();}}></div></Portal>] : []
             }      
         </div>  
